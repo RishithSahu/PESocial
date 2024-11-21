c1725d1362f7e0360e30ec05fd9abbfab12f8925
RishithSahu <rishith.sahu@gmail.com>
2024-11-21 15:07:56 +0530

    removed some notifications

--- client/src/index.js
+++ client/src/index.js
@@ -17,7 +17,6 @@ import {
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { PersistGate } from "redux-persist/integration/react";
-import { NotificationContextProvider } from "./context/NotificationContext"; // Import NotificationContextProvider
 
 const persistConfig = { key: "root", storage, version: 1 };
 const persistedReducer = persistReducer(persistConfig, authReducer);
@@ -36,9 +35,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistStore(store)}>
-        <NotificationContextProvider> {/* Wrap App with NotificationContextProvider */}
-          <App />
-        </NotificationContextProvider>
+        <App />
       </PersistGate>
     </Provider>
   </React.StrictMode>
